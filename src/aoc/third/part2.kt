009b13f82c42719e8d04fd3a05fd60e95e2782b0
mdolata <mateuszdolata2@gmail.com>
2019-12-03 21:58:08 +0100

    part2

--- src/aoc/third/part2.kt
+++ src/aoc/third/part2.kt
@@ -2,18 +2,18 @@ package aoc.third
 
 fun main() {
     val values = mapOf(
-        Pair("R8,U5,L5,D3", "U7,R6,D4,L4") to 6,
+        Pair("R8,U5,L5,D3", "U7,R6,D4,L4") to 30,
         Pair("R75,D30,R83,U83,L12,D49,R71,U7,L72",
-                "U62,R66,U55,R34,D71,R55,D58,R83") to 159,
+            "U62,R66,U55,R34,D71,R55,D58,R83") to 610,
         Pair("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
-            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") to 135
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") to 410
     )
 
     values
-        .map { Pair(solve1(it.key), it.value) }
+        .map { Pair(solve2(it.key), it.value) }
         .forEach { println(it) }
 
-    println(solve1(testCase()))
+    println(solve2(testCase()))
 
 
 }
--- src/aoc/third/solution.kt
+++ src/aoc/third/solution.kt
@@ -1,32 +1,48 @@
 package aoc.third
 
 import java.lang.RuntimeException
+import java.util.*
 import kotlin.math.abs
 
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int, val counter: Int = 0) {
+    override fun equals(other: Any?): Boolean {
+        return if (other is Point) {
+            x == other.x && y == other.y
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hashCode(x) + Objects.hashCode(y)
+    }
+}
+
 data class Step(val direction: String, val value: Int)
 
 fun solve1(data: Pair<String, String>): Int? {
-    val wire1 = data.first.split(",").map { parse(it) }
-    val wire2 = data.second.split(",").map { parse(it) }
-
-    val points1 = toPoints(wire1)
-    val points2 = toPoints(wire2)
-
-
-    return points1.intersect(points2)
-        .filter { it != Point(0,0) }
+    return getIntersections(data)
         .map { calcManhattanDistance(it) }
         .min()
-
 }
+
+fun getIntersections(data: Pair<String, String>): List<Point> {
+    val points1 = toPoints(data.first)
+    val points2 = toPoints(data.second)
+
+    return points1.intersect(points2)
+        .filter { it != Point(0, 0) }
+}
+
+private fun toPoints(data: String) =
+    toPoints(data.split(",").map { parse(it) })
 
 fun calcManhattanDistance(point: Point): Int {
     return abs(point.x) + abs(point.y)
 }
 
 private fun toPoints(wire1: List<Step>): List<Point> {
-    val lst = mutableListOf(Point(0,0))
+    val lst = mutableListOf(Point(0, 0))
     for (step in wire1) {
         val direction = step.direction
         for (i in IntRange(1, step.value)) {
@@ -40,16 +56,16 @@ private fun toPoints(wire1: List<Step>): List<Point> {
 fun moveOne(point: Point, direction: String): Point {
     return when (direction) {
         "R" -> {
-            Point(point.x + 1, point.y)
+            Point(point.x + 1, point.y, point.counter + 1)
         }
         "L" -> {
-            Point(point.x - 1, point.y)
+            Point(point.x - 1, point.y, point.counter + 1)
         }
         "U" -> {
-            Point(point.x, point.y + 1)
+            Point(point.x, point.y + 1, point.counter + 1)
         }
         "D" -> {
-            Point(point.x, point.y - 1)
+            Point(point.x, point.y - 1, point.counter + 1)
         }
         else -> throw RuntimeException()
     }
@@ -66,6 +82,17 @@ fun parse(step: String): Step {
     )
 }
 
-fun solve2(data: String): Int {
-    return 1
+fun solve2(data: Pair<String, String>): Int? {
+    val points1 = toPoints(data.first)
+    val points2 = toPoints(data.second)
+
+    val intersection = getIntersections(data)
+
+    return intersection.map { point ->
+        points1.first { p -> p == point }.counter +
+                points2.first { p -> p == point }.counter
+    }
+        .min()
+
+
 }
