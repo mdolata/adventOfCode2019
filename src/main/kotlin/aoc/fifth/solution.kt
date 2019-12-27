4b611ae769c9767f94129e9df951a9c5eadd7332
Mateusz Dolata <mateusz.dolata@ocado.com>
2019-12-27 20:06:25 +0100

    day 5 part 1 refactor, added new instructions

--- src/main/kotlin/aoc/fifth/solution.kt
+++ src/main/kotlin/aoc/fifth/solution.kt
@@ -2,62 +2,78 @@ package aoc.fifth
 
 import java.lang.RuntimeException
 
-data class Four(val opCode: Int, val in1: Int, val in2: Int, val out: Int)
+data class Instruction(val opCode: Int, val in1: Int, val in2: Int, val out: Int)
 
 fun solve1(data: String): String {
     val list = data.split(",").toMutableList()
-    var i = 0
+    var instructionPointer = 0
     do {
-        val four = takeNextFour(list, i++)
+        val instruction = takeNextInstruction(list, instructionPointer)
 
-        if (four.opCode != 99) {
-            apply(list, four)
-        }
-    } while (four.opCode != 99)
+        instructionPointer += pointerIncrementer(instruction.opCode)
+
+        apply(list, instruction)
+
+    } while (instruction.opCode != 99)
     return list.joinToString(",")
 }
 
-private fun apply(list: MutableList<String>, four: Four) {
-    when (four.opCode) {
-        1 -> {
-            list[four.out] = calc(list, four) { i1: Int, i2: Int -> i1 + i2 }
-        }
-        2 ->
-            list[four.out] = calc(list, four) { i1: Int, i2: Int -> i1 * i2 }
+private fun pointerIncrementer(opCode: Int): Int {
+    return when (opCode) {
+        1, 2 -> 4
+        3, 4 -> 2
+        else -> 0
     }
 }
 
-
-private fun calc(list: MutableList<String>, four: Four, function: (Int, Int) -> Int) =
-    function(getInt(list, four.in1), getInt(list, four.in2)).toString()
-
-
-private fun takeNextFour(data: MutableList<String>, i: Int): Four =
-    when (getInt(data, i * 4)) {
-        1 -> {
-            createFour(1, data, i)
-        }
-        2 -> {
-            createFour(2, data, i)
-        }
+private fun apply(list: MutableList<String>, instruction: Instruction) {
+    when (instruction.opCode) {
+        1 -> list[instruction.out] = calc(list, instruction) { i1: Int, i2: Int -> i1 + i2 }
+        2 -> list[instruction.out] = calc(list, instruction) { i1: Int, i2: Int -> i1 * i2 }
+        3 -> list[instruction.out] = read()
+        4 -> println("output -> ${list[instruction.out]}")
         99 -> {
-            createFour(99)
-        }
-        else -> {
-            throw RuntimeException()
         }
     }
+}
 
-private fun createFour(opCode1: Int) =
-    Four(opCode1, 0, 0, 0)
+fun read(): String {
+    println("Provide number")
+    return readLine()!!
+}
 
 
-private fun createFour(opCode1: Int, split: List<String>, i: Int) =
-    Four(opCode1, getInt(split, i * 4 + 1), getInt(split, i * 4 + 2), getInt(split, i * 4 + 3))
+private fun calc(list: MutableList<String>, instruction: Instruction, function: (Int, Int) -> Int) =
+    function(getInt(list, instruction.in1), getInt(list, instruction.in2)).toString()
 
 
-private fun getInt(split: List<String>, i: Int) =
-    Integer.parseInt(split[i])
+private fun takeNextInstruction(data: MutableList<String>, instructionPointer: Int): Instruction =
+    when (getInt(data, instructionPointer)) {
+        1 -> addition(data, instructionPointer)
+        2 -> multiplication(data, instructionPointer)
+        3 -> sysIn(data, instructionPointer)
+        4 -> sysOut(data, instructionPointer)
+        99 -> halt()
+        else -> throw RuntimeException()
+    }
+
+private fun halt() = Instruction(99, 0, 0, 0)
+private fun addition(data: List<String>, instructionPointer: Int) = createFour(1, data, instructionPointer)
+private fun multiplication(data: List<String>, instructionPointer: Int) = createFour(2, data, instructionPointer)
+private fun sysIn(data: MutableList<String>, instructionPointer: Int) = Instruction(3,0,0, getInt(data, instructionPointer + 1))
+private fun sysOut(data: MutableList<String>, instructionPointer: Int) = Instruction(4, 0,0, getInt(data,instructionPointer + 1))
+
+
+private fun createFour(opCode1: Int, split: List<String>, instructionPointer: Int) =
+    Instruction(
+        opCode1,
+        getInt(split, instructionPointer + 1),
+        getInt(split, instructionPointer + 2),
+        getInt(split, instructionPointer + 3)
+    )
+
+
+private fun getInt(data: List<String>, i: Int) = Integer.parseInt(data[i])
 
 fun solve2(data: String): Int {
     return Integer.parseInt(solve1(data).split(",")[0])
